d77f45ea5a61aec28bb16c49eb5485b6ebe256c2
katiaaperezz <katiap.perez@iteso.mx>
2025-02-18 11:42:21 -0600

    Comienza con A 1er intento

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -1,15 +1,20 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Comienza con A
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+palabra = input("Escribe una palabra: ")
 
 # Proceso
-
-
+if palabra.startswith("A"):
+    resultado = "'" + palabra + "'" + " comienza con " + "'" + "A" + "'"
+elif palabra.startswith("a"):
+    resultado = "'" + palabra + "'" + " comienza con " + "'" + "A" + "'"
+elif palabra.startswith("á"):
+    resultado = "'" + palabra + "'" + " comienza con " + "'" + "A" + "'"
+elif palabra.startswith("Á"):
+    resultado = "'" + palabra + "'" + " comienza con " + "'" + "A" + "'"
+else: 
+    resultado= "'" + palabra + "'" + " no comienza con " + "'" + "A" + "'"
 # Salidas
-print(salida)
+print(resultado)
